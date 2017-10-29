b938b505755b285fe9bd391f06057fdeb68ed63f
Scott Munday <smunday@knowware.org>
2017-10-28 23:20:28 -0400

    Added error handling.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, abort
 from flask.views import MethodView
 
 from neo4j.v1 import GraphDatabase, basic_auth
@@ -10,16 +10,18 @@ app = Flask(__name__)
 
 class JigAPI(MethodView):
     def get(self, label):
-        graph = GraphDatabase('bolt://localhost:7687', auth=basic_auth('neo4j', 'neo4j-server'))
+        try:
+            graph = GraphDatabase('bolt://localhost:7687', auth=basic_auth('neo4j', 'neo4j-server'))
 
-        with graph.session() as session:
-            result = session.run('MATCH p=(n:{label})-[r]->(m) RETURN p', label=label)
+            with graph.session() as session:
+                result = session.run('MATCH p=(n:{label})-[r]->(m) RETURN p', label=label)
 
-            for record in result:
-                pp = pprint.PrettyPrinter(indent=4)
+                for record in result:
+                    pp = pprint.PrettyPrinter(indent=4)
 
-                pp.pprint(record)
-
+                    pp.pprint(record)
+        except Exception as exception:
+            abort(400, exception)
 
 app.add_url_rule('/jig/<string:label>/', view_func=JigAPI.as_view('jig'), methods=['GET'])
 app.run(host='0.0.0.0')
